6b1b4cfe316419e4cac8d5e5ff97b6067ac44351
faizhaque <149001570+faizhaque@users.noreply.github.com>
2024-07-29 23:32:54 -0400

    Add files via upload
    Added reading the file as well as getting every single Car object.

--- main.cpp
+++ main.cpp
@@ -3,13 +3,14 @@
 #include <sstream>
 #include <vector>
 #include <string>
+#include <unordered_map>
 using namespace std;
 
 struct Car{
     vector<int> dimensions;
     string driveLine;
     string engineType;
-    bool hybrid;
+    string hybrid;
     int forwardGear;
     string transmission;
     int mpg;
@@ -23,18 +24,77 @@ struct Car{
     int horsePower;
     int torque;
 
-
 };
 
-void loadCars(unordered_map<string, Car>& cars);
-void displayMenu();
-void handleSearch(const unordered_map<string, Car>& cars);
-void handleSort(vector<Car>& cars);
-void handleFilter(const unordered_map<string, Car>& cars);
-void displayCar(const Car& car);
+vector<string> csvLines(const string& line){
+    vector<string> information;
+    string info;
+    bool quotes = false;
+    for(char c : line){
+        if (c == '"'){
+            quotes = !quotes;
+        }
+        else if(c == ',' && !quotes ){
+            information.push_back(info);
+            info.clear();
+        }
+        else{
+            info += c;
+        }
+    }
+    information.push_back(info);
+    return information;
+}
+
+vector<Car> read(const string& filename){
+    vector<Car> cars;
+    ifstream file(filename);
+    if (!file.is_open()){
+        cout << "Failed to open file" << endl;
+        return cars;
+    }
+
+    string line;
+    if (getline(file, line)) {
+
+    }
+    while (getline(file, line)){
+        vector<string> information = csvLines(line);
+        Car car;
+        car.dimensions.push_back(stoi(information[0]));
+        car.dimensions.push_back(stoi(information[1]));
+        car.dimensions.push_back(stoi(information[2]));
+        car.driveLine = information[3];
+        car.engineType = information[4];
+        car.hybrid = information[5];
+        car.forwardGear = stoi(information[6]);
+        car.transmission = information[7];
+        car.mpg = stoi(information[8]);
+        car.fuelType = information[9];
+        car.highwayMPG = stoi(information[10]);
+        car.classification = information[11];
+        car.ID = information[12];
+        car.make = information[13];
+        car.modelYear = information[14];
+        car.year = stoi(information[15]);
+        car.horsePower = stoi(information[16]);
+        car.torque = stoi(information[17]);
+        cars.push_back(car);
+    }
+    file.close();
+    return cars;
+
+}
 
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    string filename = "C:/Users/HP/CLionProjects/Project3/cars.csv";
+    vector<Car> cars = read(filename);
+    for (const auto& car : cars) {
+        std::cout << "Description: " << car.ID << ", Fuel Usage (City): " << car.mpg
+                  << ", Fuel Usage (Highway): " << car.highwayMPG
+                  << ", Torque: " << car.torque
+                  << ", Horsepower: " << car.horsePower << std::endl;
+    }
     return 0;
 }
