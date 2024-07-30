908460823fe001fc725e7f04d4394dcdf3905406
faizhaque <149001570+faizhaque@users.noreply.github.com>
2024-07-29 22:19:55 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <string>
+using namespace std;
+
+struct Car{
+    vector<int> dimensions;
+    string driveLine;
+    string engineType;
+    bool hybrid;
+    int forwardGear;
+
+
+};
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
