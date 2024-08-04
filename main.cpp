86eb1270678fe924d402f95d21fc04cfb86a247d
nmishra-ufl <128648183+nmishra-ufl@users.noreply.github.com>
2024-08-03 20:35:51 -0400

    Update main.cpp
    added sort + rudimentary listing

--- main.cpp
+++ main.cpp
@@ -85,16 +85,53 @@ vector<Car> read(const string& filename){
     return cars;
 
 }
-
+int getCategoryValue(const Car& car, const string& category) {
+    if (category == "dimensions") return car.dimensions[0] * car.dimensions[1] * car.dimensions[2];
+    if (category == "driveLine") return hash<string>{}(car.driveLine);
+    if (category == "engineType") return hash<string>{}(car.engineType);
+    if (category == "hybrid") return car.hybrid == "Yes" ? 1 : 0;
+    if (category == "forwardGear") return car.forwardGear;
+    if (category == "transmission") return hash<string>{}(car.transmission);
+    if (category == "mpg") return car.mpg;
+    if (category == "fuelType") return hash<string>{}(car.fuelType);
+    if (category == "highwayMPG") return car.highwayMPG;
+    if (category == "classification") return hash<string>{}(car.classification);
+    if (category == "ID") return hash<string>{}(car.ID);
+    if (category == "make") return hash<string>{}(car.make);
+    if (category == "modelYear") return hash<string>{}(car.modelYear);
+    if (category == "year") return car.year;
+    if (category == "horsePower") return car.horsePower;
+    if (category == "torque") return car.torque;
+    return 0;
+}
 
 int main() {
     string filename = "C:/Users/HP/CLionProjects/Project3/cars.csv";
     vector<Car> cars = read(filename);
-    for (const auto& car : cars) {
-        std::cout << "Description: " << car.ID << ", Fuel Usage (City): " << car.mpg
-                  << ", Fuel Usage (Highway): " << car.highwayMPG
-                  << ", Torque: " << car.torque
-                  << ", Horsepower: " << car.horsePower << std::endl;
+
+    vector<string> categories(4);
+    cout << "Enter 4 categories from the following list: dimensions, driveLine, engineType, hybrid, forwardGear, transmission, mpg, fuelType, highwayMPG, classification, ID, make, modelYear, year, horsePower, torque" << endl;
+    for (int i = 0; i < 4; ++i) {
+        cout << "Enter category " << i + 1 << ": ";
+        cin >> categories[i];
     }
+
+    sort(cars.begin(), cars.end(), [&categories](const Car& a, const Car& b) {
+        for (const string& category : categories) {
+            int valA = getCategoryValue(a, category);
+            int valB = getCategoryValue(b, category);
+            if (valA != valB) {
+                return valA > valB;
+            }
+        }
+        return false;
+    });
+
+    cout << "Top 20 cars based on selected categories:" << endl;
+    for (int i = 0; i < min(20, (int)cars.size()); ++i) {
+        const auto& car = cars[i];
+        cout << "Car ID: " << car.ID << ", Make: " << car.make << ", Model Year: " << car.modelYear << ", Horsepower: " << car.horsePower << ", Torque: " << car.torque << endl;
+    }
+
     return 0;
 }
