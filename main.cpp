205a58f266ea8bf498b2c55684ebfa25ac73715a
Asrith-ufl <yerramesetty.as@ufl.edu>
2024-08-03 20:35:08 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include <string>
 #include <unordered_map>
+#include <algorithm>
 using namespace std;
 
 struct Car{
@@ -24,21 +25,20 @@ struct Car{
     int horsePower;
     int torque;
 
+
 };
 
-vector<string> csvLines(const string& line){
+vector<string> csvLines(const string& line) {
     vector<string> information;
     string info;
     bool quotes = false;
-    for(char c : line){
-        if (c == '"'){
+    for (char c : line) {
+        if (c == '"') {
             quotes = !quotes;
-        }
-        else if(c == ',' && !quotes ){
+        } else if (c == ',' && !quotes) {
             information.push_back(info);
             info.clear();
-        }
-        else{
+        } else {
             info += c;
         }
     }
@@ -46,55 +46,140 @@ vector<string> csvLines(const string& line){
     return information;
 }
 
-vector<Car> read(const string& filename){
+vector<Car> read(const string& filename) {
     vector<Car> cars;
     ifstream file(filename);
-    if (!file.is_open()){
+    if (!file.is_open()) {
         cout << "Failed to open file" << endl;
         return cars;
     }
 
+
     string line;
     if (getline(file, line)) {
-
-    }
-    while (getline(file, line)){
-        vector<string> information = csvLines(line);
-        Car car;
-        car.dimensions.push_back(stoi(information[0]));
-        car.dimensions.push_back(stoi(information[1]));
-        car.dimensions.push_back(stoi(information[2]));
-        car.driveLine = information[3];
-        car.engineType = information[4];
-        car.hybrid = information[5];
-        car.forwardGear = stoi(information[6]);
-        car.transmission = information[7];
-        car.mpg = stoi(information[8]);
-        car.fuelType = information[9];
-        car.highwayMPG = stoi(information[10]);
-        car.classification = information[11];
-        car.ID = information[12];
-        car.make = information[13];
-        car.modelYear = information[14];
-        car.year = stoi(information[15]);
-        car.horsePower = stoi(information[16]);
-        car.torque = stoi(information[17]);
-        cars.push_back(car);
-    }
-    file.close();
-    return cars;
-
+}
+while (getline(file, line)) {
+vector<string> information = csvLines(line);
+Car car;
+car.dimensions.push_back(stoi(information[0]));
+car.dimensions.push_back(stoi(information[1]));
+car.dimensions.push_back(stoi(information[2]));
+car.driveLine = information[3];
+car.engineType = information[4];
+car.hybrid = information[5];
+car.forwardGear = stoi(information[6]);
+car.transmission = information[7];
+car.mpg = stoi(information[8]);
+car.fuelType = information[9];
+car.highwayMPG = stoi(information[10]);
+car.classification = information[11];
+car.ID = information[12];
+car.make = information[13];
+car.modelYear = information[14];
+car.year = stoi(information[15]);
+car.horsePower = stoi(information[16]);
+car.torque = stoi(information[17]);
+cars.push_back(car);
+}
+file.close();
+return cars;
+}
+void displayMenu() {
+    cout << "\nVehicle Selection Assistant Menu:\n";
+    cout << "1. Search for a car\n";
+    cout << "2. Sort cars\n";
+    cout << "3. Filter cars\n";
+    cout << "4. Exit\n";
 }
 
 
+void searchCar(const vector<Car>& cars) {
+    string query;
+    cout << "Enter the car ID, make, or model year to search: ";
+    cin.ignore();
+    getline(cin, query);
+
+    for (const auto& car : cars) {
+        if (car.ID == query || car.make == query || car.modelYear == query) {
+            cout << "Car found: " << car.ID << ", " << car.make << ", " << car.modelYear << endl;
+            return;
+        }
+    }
+    cout << "Car not found." << endl;
+}
+
+void sortCars(vector<Car>& cars) {
+    int sortOption;
+    cout << "Sort by: 1. Year 2. HorsePower 3. MPG\nEnter option: ";
+    cin >> sortOption;
+
+
+    switch (sortOption) {
+        case 1:
+            sort(cars.begin(), cars.end(), [](const Car& a, const Car& b) {
+                return a.year < b.year;
+            });
+            break;
+        case 2:
+            sort(cars.begin(), cars.end(), [](const Car& a, const Car& b) {
+                return a.horsePower < b.horsePower;
+            });
+            break;
+        case 3:
+            sort(cars.begin(), cars.end(), [](const Car& a, const Car& b) {
+                return a.mpg < b.mpg;
+            });
+            break;
+        default:
+            cout << "Invalid option." << endl;
+            return;
+    }
+
+    cout << "Cars sorted successfully." << endl;
+}
+
+void filterCars(const vector<Car>& cars) {
+    int filterOption;
+    cout << "Filter by: 1. Fuel Type 2. Drive Line\nEnter option: ";
+    cin >> filterOption;
+    string query;
+    cout << "Enter the value to filter: ";
+    cin.ignore();
+    getline(cin, query);
+
+    for (const auto& car : cars) {
+        if ((filterOption == 1 && car.fuelType == query) ||
+            (filterOption == 2 && car.driveLine == query)) {
+            cout << "Car: " << car.ID << ", " << car.make << ", " << car.modelYear << endl;
+        }
+    }
+}
 int main() {
     string filename = "C:/Users/HP/CLionProjects/Project3/cars.csv";
     vector<Car> cars = read(filename);
-    for (const auto& car : cars) {
-        std::cout << "Description: " << car.ID << ", Fuel Usage (City): " << car.mpg
-                  << ", Fuel Usage (Highway): " << car.highwayMPG
-                  << ", Torque: " << car.torque
-                  << ", Horsepower: " << car.horsePower << std::endl;
-    }
+    int user_choice;
+    do {
+        displayMenu();
+        cout << "Enter your choice: ";
+        cin >> user_choice;
+
+        switch (user_choice) {
+            case 1:
+                searchCar(cars);
+                break;
+            case 2:
+                sortCars(cars);
+                break;
+            case 3:
+                filterCars(cars);
+                break;
+            case 4:
+                cout << "Program Exit" << endl;
+                break;
+            default:
+                cout << "Invalid choice. Please try again." << endl;
+        }
+    } while (user_choice != 4);
+
     return 0;
 }
