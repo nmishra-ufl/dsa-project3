a0855f8d975dac181991f2a793e31fd12f2b3990
Asrith-ufl <yerramesetty.as@ufl.edu>
2024-08-05 23:47:52 -0400

    Update main.cpp
    hi

--- main.cpp
+++ main.cpp
@@ -4,10 +4,13 @@
 #include <vector>
 #include <string>
 #include <unordered_map>
+#include <unordered_set>
 #include <algorithm>
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
 using namespace std;
 
-struct Car{
+struct Car {
     vector<int> dimensions;
     string driveLine;
     string engineType;
@@ -24,8 +27,6 @@ struct Car{
     int year;
     int horsePower;
     int torque;
-
-
 };
 
 vector<string> csvLines(const string& line) {
@@ -35,10 +36,12 @@ vector<string> csvLines(const string& line) {
     for (char c : line) {
         if (c == '"') {
             quotes = !quotes;
-        } else if (c == ',' && !quotes) {
+        }
+        else if (c == ',' && !quotes) {
             information.push_back(info);
             info.clear();
-        } else {
+        }
+        else {
             info += c;
         }
     }
@@ -46,7 +49,12 @@ vector<string> csvLines(const string& line) {
     return information;
 }
 
-vector<Car> read(const string& filename) {
+vector<Car> read(const string& filename,
+                 unordered_map<string, vector<Car>>& makeMap,
+                 unordered_map<string, vector<Car>>& modelYearMap,
+                 unordered_map<int, vector<Car>>& yearMap,
+                 unordered_map<int, vector<Car>>& horsePowerMap,
+                 unordered_map<int, vector<Car>>& forwardGearMap) {
     vector<Car> cars;
     ifstream file(filename);
     if (!file.is_open()) {
@@ -54,132 +62,235 @@ vector<Car> read(const string& filename) {
         return cars;
     }
 
-
     string line;
     if (getline(file, line)) {
-}
-while (getline(file, line)) {
-vector<string> information = csvLines(line);
-Car car;
-car.dimensions.push_back(stoi(information[0]));
-car.dimensions.push_back(stoi(information[1]));
-car.dimensions.push_back(stoi(information[2]));
-car.driveLine = information[3];
-car.engineType = information[4];
-car.hybrid = information[5];
-car.forwardGear = stoi(information[6]);
-car.transmission = information[7];
-car.mpg = stoi(information[8]);
-car.fuelType = information[9];
-car.highwayMPG = stoi(information[10]);
-car.classification = information[11];
-car.ID = information[12];
-car.make = information[13];
-car.modelYear = information[14];
-car.year = stoi(information[15]);
-car.horsePower = stoi(information[16]);
-car.torque = stoi(information[17]);
-cars.push_back(car);
-}
-file.close();
-return cars;
-}
-void displayMenu() {
-    cout << "\nVehicle Selection Assistant Menu:\n";
-    cout << "1. Search for a car\n";
-    cout << "2. Sort cars\n";
-    cout << "3. Filter cars\n";
-    cout << "4. Exit\n";
+    }
+    while (getline(file, line)) {
+        vector<string> information = csvLines(line);
+
+        Car car;
+        car.dimensions.push_back(stoi(information[0]));
+        car.dimensions.push_back(stoi(information[1]));
+        car.dimensions.push_back(stoi(information[2]));
+        car.driveLine = information[3];
+        car.engineType = information[4];
+        car.hybrid = information[5];
+        car.forwardGear = stoi(information[6]);
+        car.transmission = information[7];
+        car.mpg = stoi(information[8]);
+        car.fuelType = information[9];
+        car.highwayMPG = stoi(information[10]);
+        car.classification = information[11];
+        car.ID = information[12];
+        car.make = information[13];
+        car.modelYear = information[14];
+        car.year = stoi(information[15]);
+        car.horsePower = stoi(information[16]);
+        car.torque = stoi(information[17]);
+
+        makeMap[car.make].push_back(car);
+        modelYearMap[car.modelYear].push_back(car);
+        yearMap[car.year].push_back(car);
+        horsePowerMap[car.horsePower].push_back(car);
+        forwardGearMap[car.forwardGear].push_back(car);
+
+        cars.push_back(car);
+    }
+    file.close();
+    return cars;
 }
 
+double calculateWeightedSum(const Car& car, const unordered_map<string, double>& weights) {
+    double sum = 0.0;
+    sum += car.mpg * weights.at("mpg");
+    sum += car.highwayMPG * weights.at("highwayMPG");
+    sum += car.year * weights.at("year");
+    sum += car.horsePower * weights.at("horsePower");
+    sum += car.torque * weights.at("torque");
+    return sum;
+}
 
-void searchCar(const vector<Car>& cars) {
-    string query;
-    cout << "Enter the car ID, make, or model year to search: ";
-    cin.ignore();
-    getline(cin, query);
+void rankCars(vector<Car>& cars, const unordered_map<string, double>& weights) {
+    sort(cars.begin(), cars.end(), [&weights](const Car& a, const Car& b) {
+        return calculateWeightedSum(a, weights) > calculateWeightedSum(b, weights);
+    });
+}
 
-    for (const auto& car : cars) {
-        if (car.ID == query || car.make == query || car.modelYear == query) {
-            cout << "Car found: " << car.ID << ", " << car.make << ", " << car.modelYear << endl;
-            return;
+vector<Car> correctResults(const vector<Car>& base, const vector<Car>& additional) {
+    unordered_set<string> baseNew;
+    for (const auto& car : base) {
+        baseNew.insert(car.ID);
+    }
+    vector<Car> result;
+    for (const auto& car : additional) {
+        if (baseNew.find(car.ID) != baseNew.end()) {
+            result.push_back(car);
         }
     }
-    cout << "Car not found." << endl;
+    return result;
 }
 
-void sortCars(vector<Car>& cars) {
-    int sortOption;
-    cout << "Sort by: 1. Year 2. HorsePower 3. MPG\nEnter option: ";
-    cin >> sortOption;
+vector<Car> searchCars(const unordered_map<string, vector<Car>>& makeMap,
+                       const unordered_map<string, vector<Car>>& modelYearMap,
+                       const unordered_map<int, vector<Car>>& yearMap,
+                       const unordered_map<int, vector<Car>>& horsePowerMap,
+                       const unordered_map<int, vector<Car>>& forwardGearMap,
+                       const unordered_map<string, string>& criteria,
+                       const unordered_map<string, int>& criteriaInt) {
+    vector<Car> results;
+    bool first = true;
 
-
-    switch (sortOption) {
-        case 1:
-            sort(cars.begin(), cars.end(), [](const Car& a, const Car& b) {
-                return a.year < b.year;
-            });
-            break;
-        case 2:
-            sort(cars.begin(), cars.end(), [](const Car& a, const Car& b) {
-                return a.horsePower < b.horsePower;
-            });
-            break;
-        case 3:
-            sort(cars.begin(), cars.end(), [](const Car& a, const Car& b) {
-                return a.mpg < b.mpg;
-            });
-            break;
-        default:
-            cout << "Invalid option." << endl;
-            return;
-    }
-
-    cout << "Cars sorted successfully." << endl;
-}
-
-void filterCars(const vector<Car>& cars) {
-    int filterOption;
-    cout << "Filter by: 1. Fuel Type 2. Drive Line\nEnter option: ";
-    cin >> filterOption;
-    string query;
-    cout << "Enter the value to filter: ";
-    cin.ignore();
-    getline(cin, query);
-
-    for (const auto& car : cars) {
-        if ((filterOption == 1 && car.fuelType == query) ||
-            (filterOption == 2 && car.driveLine == query)) {
-            cout << "Car: " << car.ID << ", " << car.make << ", " << car.modelYear << endl;
+    if (criteria.find("make") != criteria.end()) {
+        auto it = makeMap.find(criteria.at("make"));
+        if (it != makeMap.end()) {
+            if (first) {
+                results = it->second;
+                first = false;
+            }
+            else {
+                results = correctResults(results, it->second);
+            }
         }
     }
+    if (criteria.find("modelYear") != criteria.end()) {
+        auto it = modelYearMap.find(criteria.at("modelYear"));
+        if (it != modelYearMap.end()) {
+            if (first) {
+                results = it->second;
+                first = false;
+            } else {
+                results = correctResults(results, it->second);
+            }
+        }
+    }
+    if (criteriaInt.find("year") != criteriaInt.end()) {
+        auto it = yearMap.find(criteriaInt.at("year"));
+        if (it != yearMap.end()) {
+            if (first) {
+                results = it->second;
+                first = false;
+            } else {
+                results = correctResults(results, it->second);
+            }
+        }
+    }
+    if (criteriaInt.find("horsePower") != criteriaInt.end()) {
+        auto it = horsePowerMap.find(criteriaInt.at("horsePower"));
+        if (it != horsePowerMap.end()) {
+            if (first) {
+                results = it->second;
+                first = false;
+            } else {
+                results = correctResults(results, it->second);
+            }
+        }
+    }
+    if (criteriaInt.find("forwardGear") != criteriaInt.end()) {
+        auto it = forwardGearMap.find(criteriaInt.at("forwardGear"));
+        if (it != forwardGearMap.end()) {
+            if (first) {
+                results = it->second;
+                first = false;
+            } else {
+                results = correctResults(results, it->second);
+            }
+        }
+    }
+
+    return results;
 }
+
+void printCars(const vector<Car>& cars) {
+    for (const auto& car : cars) {
+        cout << "ID: " << car.ID << ", Make: " << car.make << ", Model Year: " << car.modelYear
+             << ", MPG: " << car.mpg << ", Highway MPG: " << car.highwayMPG << ", Year: " << car.year
+             << ", HorsePower: " << car.horsePower << ", Torque: " << car.torque << endl;
+    }
+}
+
 int main() {
-    string filename = "C:/Users/HP/CLionProjects/Project3/cars.csv";
-    vector<Car> cars = read(filename);
-    int user_choice;
-    do {
-        displayMenu();
-        cout << "Enter your choice: ";
-        cin >> user_choice;
+    string filename = "C:/Users/asrit/Downloads/cars.csv";
 
-        switch (user_choice) {
-            case 1:
-                searchCar(cars);
-                break;
-            case 2:
-                sortCars(cars);
-                break;
-            case 3:
-                filterCars(cars);
-                break;
-            case 4:
-                cout << "Program Exit" << endl;
-                break;
-            default:
-                cout << "Invalid choice. Please try again." << endl;
+    unordered_map<string, vector<Car>> makeMap;
+    unordered_map<string, vector<Car>> modelYearMap;
+    unordered_map<int, vector<Car>> yearMap;
+    unordered_map<int, vector<Car>> horsePowerMap;
+    unordered_map<int, vector<Car>> forwardGearMap;
+
+    vector<Car> cars = read(filename, makeMap, modelYearMap, yearMap, horsePowerMap, forwardGearMap);
+
+
+    sf::RenderWindow window(sf::VideoMode(800, 600), "AutoSearch Vehicle Selection Assistant", sf::Style::Close);
+    sf::Font font;
+    if (!font.loadFromFile("font.ttf")) {
+        cerr << "Failed to load font" << endl;
+        return -1;
+    }
+
+    sf::Text instructions("Press 'R' to rank cars and display top 20.\nPress 'S' to search cars based on criteria.\nPress 'Esc' to exit.", font, 20);
+    instructions.setFillColor(sf::Color::White);
+    instructions.setPosition(10, 10);
+
+    bool displayTop20 = false;
+    bool searchMode = false;
+    vector<Car> searchResults;
+
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed || (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape)) {
+                window.close();
+            }
+            if (event.type == sf::Event::KeyPressed) {
+                if (event.key.code == sf::Keyboard::R) {
+                    displayTop20 = true;
+                    searchMode = false;
+                }
+                if (event.key.code == sf::Keyboard::S) {
+                    displayTop20 = false;
+                    searchMode = true;
+                }
+            }
         }
-    } while (user_choice != 4);
+
+        window.clear();
+
+        if (displayTop20) {
+            rankCars(cars, {
+                    {"mpg", 1.0},
+                    {"highwayMPG", 0.8},
+                    {"year", 0.5},
+                    {"horsePower", 0.7},
+                    {"torque", 0.6}
+            });
+
+            sf::Text carText("", font, 15);
+            carText.setFillColor(sf::Color::White);
+            float yPosition = 50.0f;
+
+            for (size_t i = 0; i < 20 && i < cars.size(); ++i) {
+                carText.setString("ID: " + cars[i].ID + ", Make: " + cars[i].make + ", Model Year: " + cars[i].modelYear);
+                carText.setPosition(10, yPosition);
+                window.draw(carText);
+                yPosition += 20;
+            }
+        } else if (searchMode) {
+           
+            sf::Text searchText("Search Mode Activated. Implement search criteria input here.", font, 20);
+            searchText.setFillColor(sf::Color::White);
+            searchText.setPosition(10, 50);
+            window.draw(searchText);
+        } else {
+            window.draw(instructions);
+        }
+
+        window.display();
+    }
 
     return 0;
 }
+
+ 
+
+
+
