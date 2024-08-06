0630266dc4c52564a02ae0a9843814d92b13fcf6
faizhaque <149001570+faizhaque@users.noreply.github.com>
2024-08-05 20:44:31 -0400

    Add files via upload
    Changed the search method to try and incorporate a hash-based search instead. Optimized new functions and added a bit menu-wise.

--- main.cpp
+++ main.cpp
@@ -4,9 +4,10 @@
 #include <vector>
 #include <string>
 #include <unordered_map>
+#include <unordered_set>
 using namespace std;
 
-struct Car{
+struct Car {
     vector<int> dimensions;
     string driveLine;
     string engineType;
@@ -23,22 +24,21 @@ struct Car{
     int year;
     int horsePower;
     int torque;
-
 };
 
-vector<string> csvLines(const string& line){
+vector<string> csvLines(const string& line) {
     vector<string> information;
     string info;
     bool quotes = false;
-    for(char c : line){
-        if (c == '"'){
+    for (char c : line) {
+        if (c == '"') {
             quotes = !quotes;
         }
-        else if(c == ',' && !quotes ){
+        else if (c == ',' && !quotes) {
             information.push_back(info);
             info.clear();
         }
-        else{
+        else {
             info += c;
         }
     }
@@ -46,20 +46,25 @@ vector<string> csvLines(const string& line){
     return information;
 }
 
-vector<Car> read(const string& filename){
+vector<Car> read(const string& filename,
+                 unordered_map<string, vector<Car>>& makeMap,
+                 unordered_map<string, vector<Car>>& modelYearMap,
+                 unordered_map<int, vector<Car>>& yearMap,
+                 unordered_map<int, vector<Car>>& horsePowerMap,
+                 unordered_map<int, vector<Car>>& forwardGearMap) {
     vector<Car> cars;
     ifstream file(filename);
-    if (!file.is_open()){
+    if (!file.is_open()) {
         cout << "Failed to open file" << endl;
         return cars;
     }
 
     string line;
     if (getline(file, line)) {
-
     }
-    while (getline(file, line)){
+    while (getline(file, line)) {
         vector<string> information = csvLines(line);
+
         Car car;
         car.dimensions.push_back(stoi(information[0]));
         car.dimensions.push_back(stoi(information[1]));
@@ -79,22 +84,152 @@ vector<Car> read(const string& filename){
         car.year = stoi(information[15]);
         car.horsePower = stoi(information[16]);
         car.torque = stoi(information[17]);
+
+        makeMap[car.make].push_back(car);
+        modelYearMap[car.modelYear].push_back(car);
+        yearMap[car.year].push_back(car);
+        horsePowerMap[car.horsePower].push_back(car);
+        forwardGearMap[car.forwardGear].push_back(car);
+
         cars.push_back(car);
     }
     file.close();
     return cars;
-
 }
 
+vector<Car> correctResults(const vector<Car>& base, const vector<Car>& additional) {
+    unordered_set<string> baseNew;
+    for (const auto& car : base) {
+        baseNew.insert(car.ID);
+    }
+    vector<Car> result;
+    for (const auto& car : additional) {
+        if (baseNew.find(car.ID) != baseNew.end()) {
+            result.push_back(car);
+        }
+    }
+    return result;
+}
+
+vector<Car> searchCars(const unordered_map<string, vector<Car>>& makeMap,
+                       const unordered_map<string, vector<Car>>& modelYearMap,
+                       const unordered_map<int, vector<Car>>& yearMap,
+                       const unordered_map<int, vector<Car>>& horsePowerMap,
+                       const unordered_map<int, vector<Car>>& forwardGearMap,
+                       const unordered_map<string, string>& criteria,
+                       const unordered_map<string, int>& criteriaInt) {
+    vector<Car> results;
+    bool first = true;
+
+    if (criteria.find("make") != criteria.end()) {
+        auto it = makeMap.find(criteria.at("make"));
+        if (it != makeMap.end()) {
+            if (first) {
+                results = it->second;
+                first = false;
+            }
+            else {
+                results = correctResults(results, it->second);
+            }
+        }
+    }
+    if (criteria.find("modelYear") != criteria.end()) {
+        auto it = modelYearMap.find(criteria.at("modelYear"));
+        if (it != modelYearMap.end()) {
+            if (first) {
+                results = it->second;
+                first = false;
+            } else {
+                results = correctResults(results, it->second);
+            }
+        }
+    }
+    if (criteriaInt.find("year") != criteriaInt.end()) {
+        auto it = yearMap.find(criteriaInt.at("year"));
+        if (it != yearMap.end()) {
+            if (first) {
+                results = it->second;
+                first = false;
+            } else {
+                results = correctResults(results, it->second);
+            }
+        }
+    }
+    if (criteriaInt.find("horsePower") != criteriaInt.end()) {
+        auto it = horsePowerMap.find(criteriaInt.at("horsePower"));
+        if (it != horsePowerMap.end()) {
+            if (first) {
+                results = it->second;
+                first = false;
+            } else {
+                results = correctResults(results, it->second);
+            }
+        }
+    }
+    if (criteriaInt.find("forwardGear") != criteriaInt.end()) {
+        auto it = forwardGearMap.find(criteriaInt.at("forwardGear"));
+        if (it != forwardGearMap.end()) {
+            if (first) {
+                results = it->second;
+                first = false;
+            } else {
+                results = correctResults(results, it->second);
+            }
+        }
+    }
+
+    return results;
+}
+
+void printCars(const vector<Car>& cars) {
+    for (const auto& car : cars) {
+        cout << "ID: " << car.ID << ", Make: " << car.make << ", Model Year: " << car.modelYear
+             << ", MPG: " << car.mpg << ", Highway MPG: " << car.highwayMPG << ", Year: " << car.year
+             << ", HorsePower: " << car.horsePower << ", Torque: " << car.torque << endl;
+    }
+}
 
 int main() {
     string filename = "C:/Users/HP/CLionProjects/Project3/cars.csv";
-    vector<Car> cars = read(filename);
-    for (const auto& car : cars) {
-        std::cout << "Description: " << car.ID << ", Fuel Usage (City): " << car.mpg
-                  << ", Fuel Usage (Highway): " << car.highwayMPG
-                  << ", Torque: " << car.torque
-                  << ", Horsepower: " << car.horsePower << std::endl;
-    }
+
+    unordered_map<string, vector<Car>> makeMap;
+    unordered_map<string, vector<Car>> modelYearMap;
+    unordered_map<int, vector<Car>> yearMap;
+    unordered_map<int, vector<Car>> horsePowerMap;
+    unordered_map<int, vector<Car>> forwardGearMap;
+
+    vector<Car> cars = read(filename, makeMap, modelYearMap, yearMap, horsePowerMap, forwardGearMap);
+
+    cout << "Welcome to the AutoSearch Vehicle Compendium!" << endl;
+
+    unordered_map<string, double> weights = {
+            {"mpg", 1.0},
+            {"highwayMPG", 0.8},
+            {"year", 0.5},
+            {"horsePower", 0.7},
+            {"torque", 0.6}
+    };
+
+    cout << "Here are the current Top 20 Cars Ranked:" << endl;
+    printCars(vector<Car>(cars.begin(), cars.begin() + 20));
+    cout << "" << endl;
+    cout << "Please select your search criteria" << endl;
+    cout << "You can choose up to four categories" << endl;
+    cout << "Here are the categories: DriveLine, Engine Type, Hybrid, "
+            "Number of Forward Gears, Transmission, MPG, Fuel Type, "
+            "Highway MPG, Classification, Make, Model Year, "
+            "Year, Horsepower, Torque." << endl;
+
+    unordered_map<string, string> searchCriteria = {
+            {"make", "Toyota"}
+    };
+    unordered_map<string, int> intSearchCriteria = {
+            {"year", 2012}
+    };
+
+    vector<Car> searchResults = searchCars(makeMap, modelYearMap, yearMap, horsePowerMap, forwardGearMap, searchCriteria, intSearchCriteria);
+    cout << "\nSearch Results:" << endl;
+    printCars(searchResults);
+
     return 0;
 }
