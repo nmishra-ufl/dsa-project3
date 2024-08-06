2694f63cda311368b48af7e4e962cb098665c64d
Asrith-ufl <yerramesetty.as@ufl.edu>
2024-08-06 01:51:34 -0400

    Update main.cpp
    Added more menu functionality to the GUI.

--- main.cpp
+++ main.cpp
@@ -276,12 +276,12 @@ int main() {
 
     // Text for displaying welcome message and options
     sf::Text welcomeText("Welcome to the AutoSearch Vehicle Selection Assistant!\n\n"
+                         "Press 'R' to input ranking criteria.\n"
                          "Press 'I' to input search criteria.\n"
                          "Press 'Esc' to exit.", font, 24);
     welcomeText.setFillColor(sf::Color::White);
     setText(welcomeText, window.getSize().x / 2.0f, window.getSize().y / 2.0f - 100);
 
-    // Text for displaying user input
     string userInput;
     sf::Text inputText("", font, 18);
     inputText.setFillColor(sf::Color::Yellow);
@@ -307,6 +307,7 @@ int main() {
                     if (event.key.code == sf::Keyboard::R) {
                         currentState = RANKING;
                         needsUpdate = true;
+
                     } else if (event.key.code == sf::Keyboard::I){
                         currentState = INPUT;
                         needsUpdate = true;
@@ -409,126 +410,3 @@ int main() {
 
     return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*int main() {
-    string filename = "C:/Users/asrit/Downloads/cars.csv";
-
-    unordered_map<string, vector<Car>> makeMap;
-    unordered_map<string, vector<Car>> modelYearMap;
-    unordered_map<int, vector<Car>> yearMap;
-    unordered_map<int, vector<Car>> horsePowerMap;
-    unordered_map<int, vector<Car>> forwardGearMap;
-
-    vector<Car> cars = read(filename, makeMap, modelYearMap, yearMap, horsePowerMap, forwardGearMap);
-
-
-    sf::RenderWindow window(sf::VideoMode(1700, 800), "AutoSearch Vehicle Selection Assistant", sf::Style::Close);
-    sf::Font font;
-    if (!font.loadFromFile("font.ttf")) {
-        cerr << "Failed to load font" << endl;
-        return -1;
-    }
-
-    sf::Text instructions("Press 'R' to rank cars and display top 20.\nPress 'S' to search cars based on criteria.\nPress 'Esc' to exit.", font, 20);
-    instructions.setFillColor(sf::Color::White);
-    instructions.setPosition(10, 10);
-
-    sf::Text rankingText("Top 20 Cars (Ranked):\n\n... (Car details here)\n\n"
-                         "Press 'B' to go back to the main menu.\n"
-                         "Press 'Esc' to exit.", font, 24);
-    rankingText.setFillColor(sf::Color::White);
-    rankingText.setPosition(10, 10);
-
-    sf::Text searchText("Search Mode Activated.\n\n... (Search criteria input here)\n\n"
-                        "Press 'B' to go back to the main menu.\n"
-                        "Press 'Esc' to exit.", font, 24);
-    searchText.setFillColor(sf::Color::White);
-    searchText.setPosition(10, 10);
-
-    bool displayTop20 = false;
-    bool searchMode = false;
-    vector<Car> searchResults;
-
-    while (window.isOpen()) {
-        sf::Event event;
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed || (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape)) {
-                window.close();
-            }
-            if (event.type == sf::Event::KeyPressed) {
-                if (event.key.code == sf::Keyboard::R) {
-                    displayTop20 = true;
-                    searchMode = false;
-                }
-                if (event.key.code == sf::Keyboard::S) {
-                    displayTop20 = false;
-                    searchMode = true;
-                }
-            }
-        }
-
-        window.clear(sf::Color::Blue);
-
-        if (displayTop20) {
-            rankCars(cars, {
-                    {"mpg", 1.0},
-                    {"highwayMPG", 0.8},
-                    {"year", 0.5},
-                    {"horsePower", 0.7},
-                    {"torque", 0.6}
-            });
-
-            sf::Text carText("", font, 18);
-            carText.setFillColor(sf::Color::White);
-            float yPosition = 50.0f;
-            float maxWidth = window.getSize().x - 20;
-
-
-            for (size_t i = 0; i < 20 && i < cars.size(); ++i) {
-                string carDetails = "ID: " + cars[i].ID + ", Make: " + cars[i].make + ", Model Year: " + cars[i].modelYear +
-                        ", MPG: " + to_string(cars[i].mpg) + ", Highway MPG: " + to_string(cars[i].highwayMPG) +
-                        ", Year: " + to_string(cars[i].year) + ", HorsePower: " + to_string(cars[i].horsePower) +
-                        ", Torque: " + to_string(cars[i].torque);
-                //carText.setPosition(10, yPosition);
-                //window.draw(carText);
-                //yPosition += 20;
-                vector<string> wrappedText = wrapText(carDetails, font, 18, maxWidth);
-                for (const auto &line: wrappedText) {
-                    carText.setString(line);
-                    carText.setPosition(10, yPosition);
-                    window.draw(carText);
-                    yPosition += 30;
-                }
-                yPosition += 10;
-
-            }
-        } else if (searchMode) {
-
-            sf::Text searchText("Search Mode Activated. Implement search criteria input here.", font, 20);
-            searchText.setFillColor(sf::Color::White);
-            searchText.setPosition(10, 50);
-            window.draw(searchText);
-        } else {
-            window.draw(instructions);
-        }
-
-        window.display();
-    }
-
-    return 0;
-}*/
